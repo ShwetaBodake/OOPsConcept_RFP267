064b2d656abcfa1f6f913e457c0357c93925be6e
Shweta Bodake <shwetabodake8108@gmail.com>
2023-03-16 18:24:03 +0530

    [Shweta] Welcome message is displayed

---
+++ OOPsConcept_RFP267/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OOPsConcept_RFP267
+{
+public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to OOPs Concept Programs");
+        }
+    }
+}
+
